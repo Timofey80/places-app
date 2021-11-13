bb97a595ceb452a033dc5dfb7f09d6ad25a030ae
Timofey <timofey.official.1@gmail.com>
2021-11-13 21:20:19 +0200

    upload to github

---
+++ public/precache-manifest.f50a37ff8fc457885bce0c9f70319393.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "332e7d3e5ab5832ff324c1a315c86d60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecbbf34c3fe892a2da61",
+    "url": "/static/css/main.ac4974b9.chunk.css"
+  },
+  {
+    "revision": "55f0548578dbea4e147a",
+    "url": "/static/js/2.c675cb09.chunk.js"
+  },
+  {
+    "revision": "ecbbf34c3fe892a2da61",
+    "url": "/static/js/main.736b359d.chunk.js"
+  },
+  {
+    "revision": "51810371bf2cf35f1e22",
+    "url": "/static/js/runtime-main.85498786.js"
+  }
+]);
